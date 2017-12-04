2acf617ca7ba4cd9eb239a039286761b449011a9
oligus <oli.gustafsson@gmail.com>
2017-12-04 17:39:10 +0100

    Advent of code 2017

--- spec/2017/4/Aoc.spec.php
+++ spec/2017/4/Aoc.spec.php
@@ -8,7 +8,32 @@ describe('Advent of code', function() {
     $aoc = new Aoc();
 
     it('Test run part 1', function() use ($aoc) {
-        expect($aoc->run(1))->toEql('day4');
+        expect($aoc->run(1))->toEql(466);
     });
 
+    it('Test run part 2', function() use ($aoc) {
+        expect($aoc->run(2))->toEql(251);
+    });
+
+    it('Test remove duplicates', function() use ($aoc) {
+        expect($aoc->countUnique([
+            'aa', 'bb', 'cc', 'dd', 'aa'
+        ]))->toEql(3);
+    });
+
+    it('Test is anagram', function() use ($aoc) {
+        expect($aoc->isAnagram('abcde', 'ecdab'))->toEql(true);
+        expect($aoc->isAnagram('oooi', 'iooo'))->toEql(true);
+        expect($aoc->isAnagram('iooi', 'iooo'))->toEql(false);
+    });
+
+    it('Test has anagram', function() use ($aoc) {
+        expect($aoc->hasAnagram(['abcde', 'ebcda', 'moo', 'jii']))->toEql(true);
+        expect($aoc->hasAnagram(['abcde','fghij']))->toEql(false);
+        expect($aoc->hasAnagram(['abcde', 'xyz', 'ecdab']))->toEql(true);
+        expect($aoc->hasAnagram(['a','ab','abc','abd','abf','abj']))->toEql(false);
+        expect($aoc->hasAnagram(['iiii','oiii','ooii','oooi','oooo']))->toEql(false);
+        expect($aoc->hasAnagram(['oiii','ioii','iioi','iiio']))->toEql(true);
+        expect($aoc->hasAnagram(['apg','gbycwe','gap','pag']))->toEql(true);
+    });
 });
--- src/2017/4/Aoc.php
+++ src/2017/4/Aoc.php
@@ -8,8 +8,71 @@ use AOC\Common\AbstractAoc;
 
 class Aoc extends AbstractAoc
 {
+    public function countUnique(array $data): int
+    {
+        $result = 0;
+
+        foreach(array_count_values($data) as $key => $value) {
+            if($value === 1) {
+                $result++;
+            }
+        }
+
+       return $result;
+    }
+
+    public function isAnagram($str1, $str2)
+    {
+        $r1 = count_chars($str1, 1);
+        $r2 = count_chars($str2, 1);
+        return $r1 === $r2;
+    }
+
+    public function hasAnagram($row)
+    {
+        foreach($row as $key => $value) {
+            for($i = 0; $i < count($row); $i++) {
+                if($i === $key) {
+                    continue;
+                }
+
+                if($this->isAnagram($row[$i], $value)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     public function run($part = 1)
     {
-        return "day4";
+        $input = $this->readInputFile(dirname(__FILE__));
+
+        if($part === 1) {
+            $result = 0;
+
+            foreach ($input as $row) {
+                $row = preg_split('/\s/', trim($row));
+
+                if($this->countUnique($row) === count($row)) {
+                    $result++;
+                }
+            }
+
+            return $result;
+        } else {
+            $invalid = 0;
+
+            foreach ($input as $key=> $row) {
+                $row = preg_split('/\s/', trim($row));
+
+                if($this->hasAnagram($row)) {
+                    $invalid++;
+                }
+            }
+
+            return count($input) - $invalid;
+        }
     }
 }
