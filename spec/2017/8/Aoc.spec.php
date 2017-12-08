4ed8f618212e7c2f7457a65a939ad9b78ba45881
oligus <oli.gustafsson@gmail.com>
2017-12-08 18:11:15 +0100

    Day 8

--- spec/2017/8/Aoc.spec.php
+++ spec/2017/8/Aoc.spec.php
@@ -7,8 +7,65 @@ use AOC\Aoc2017\Day8\Aoc;
 describe('Advent of code', function() {
     $aoc = new Aoc();
 
-    it('Test run part 1', function() use ($aoc) {
-        expect($aoc->run(1))->toEql('day8');
+    $input = [
+        'b inc 5 if a > 1',
+        'a inc 1 if b < 5',
+        'c dec -10 if a >= 1',
+        'c inc -20 if c == 10'
+    ];
+
+    beforeEach(function() use ($aoc) {
+        $aoc->setRegistry([]);
     });
+
+    it('Test run part 1', function() use ($aoc) {
+        expect($aoc->run(1))->toEql(6828);
+    });
+
+    it('Test run part 2', function() use ($aoc) {
+        expect($aoc->run(2))->toEql(7234);
+    });
+
+
+    it('Test set instructions', function() use ($aoc, $input) {
+        $aoc->setInstruction($input);
+        expect($aoc->getRegistry())->toEql(['a' => 1, 'b' => 0, 'c' => -10]);
+
+        // utc dec -736 if p > -7
+        $aoc->setInstruction(['utc dec -736 if p > -7']);
+        expect($aoc->getRegistry())->toEql(['a' => 1, 'b' => 0, 'c' => -10, 'p' => 0, 'utc' => 736]);
+
+        // hkt inc 875 if m != -6
+        $aoc->setInstruction(['hkt inc 875 if m != -6']);
+    });
+
+    it('Test get modify registry', function() use ($aoc, $input) {
+        $aoc->setRegistry(['a' => 3, 'b' => 2]);
+        $aoc->modifyRegistry('b', 'inc', 5, 'a', '>', 2);
+        expect($aoc->getRegistry())->toEql(['a' => 3, 'b' => 7]);
+
+        $aoc->setRegistry(['a' => 1, 'b' => 0, 'c' => 0]);
+        $aoc->modifyRegistry('c', 'dec', -10, 'a', '>=', 1);
+        expect($aoc->getRegistry())->toEql(['a' => 1, 'b' => 0, 'c' => 10]);
+    });
+
+    it('Test update', function() use ($aoc, $input) {
+        $aoc->setRegistry(['a' => 1, 'b' => 0]);
+        $aoc->update('a', 'inc', -10);
+        expect($aoc->getRegistry())->toEql(['a' => -9, 'b' => 0]);
+        $aoc->update('b', 'dec', 10);
+        expect($aoc->getRegistry())->toEql(['a' => -9, 'b' => -10]);
+        $aoc->update('b', 'inc', 10);
+        expect($aoc->getRegistry())->toEql(['a' => -9, 'b' => 0]);
+        $aoc->update('b', 'inc', -10);
+        expect($aoc->getRegistry())->toEql(['a' => -9, 'b' => -10]);
+    });
+
+    it('Test get highest', function() use ($aoc, $input) {
+        $aoc->setRegistry([]);
+        $aoc->setInstruction($input);
+        expect($aoc->getHighest())->toEql(1);
+    });
+
 
 });
--- src/2017/8/Aoc.php
+++ src/2017/8/Aoc.php
@@ -8,8 +8,169 @@ use AOC\Common\AbstractAoc;
 
 class Aoc extends AbstractAoc
 {
+    /**
+     * @var array
+     */
+    private $registry = [];
+
+    /**
+     * @var int
+     */
+    private $allTimeHigh = 0;
+
+    /**
+     * @param array $registry
+     */
+    public function setRegistry(array $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRegistry()
+    {
+        return $this->registry;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAllTimeHigh(): int
+    {
+        return $this->allTimeHigh;
+    }
+
+    /**
+     * @param $input
+     * @throws \Exception
+     */
+    public function setInstruction($input)
+    {
+        foreach($input as $row) {
+            preg_match('/^(\w+)\s(inc|dec)\s(-?\d+)\s(if)\s(\w+)\s([=<>!]{1,2})\s(-?\d+)$/', $row, $matches);
+
+            $target = $matches[1];
+            $modifier = $matches[2];
+            $targetValue = (int) $matches[3];
+
+            $registry = $matches[5];
+            $operator= $matches[6];
+            $value = (int) $matches[7];
+
+            $this->modifyRegistry($target, $modifier, $targetValue, $registry, $operator, $value);
+        }
+    }
+
+    /**
+     * @param $target
+     * @param $modifier
+     * @param $targetValue
+     * @param $registry
+     * @param $operator
+     * @param $value
+     * @throws \Exception
+     */
+    public function modifyRegistry($target, $modifier, $targetValue, $registry, $operator, $value)
+    {
+
+        if(!array_key_exists($registry, $this->registry)) {
+            $this->registry[$registry] = 0;
+        }
+
+        if(!array_key_exists($target, $this->registry)) {
+            $this->registry[$target] = 0;
+        }
+
+        switch($operator) {
+            case '>':
+                if($this->registry[$registry] > $value) {
+                    $this->update($target, $modifier, $targetValue);
+                }
+                break;
+
+            case '>=':
+                if($this->registry[$registry] >= $value) {
+                    $this->update($target, $modifier, $targetValue);
+                }
+                break;
+
+            case '<':
+                if($this->registry[$registry] < $value) {
+                    $this->update($target, $modifier, $targetValue);
+                }
+                break;
+
+            case '<=':
+                if($this->registry[$registry] <= $value) {
+                    $this->update($target, $modifier, $targetValue);
+                }
+                break;
+
+            case '==':
+                if($this->registry[$registry] == $value) {
+                    $this->update($target, $modifier, $targetValue);
+                }
+                break;
+
+            case '!=':
+                if($this->registry[$registry] != $value) {
+                    $this->update($target, $modifier, $targetValue);
+                }
+                break;
+
+            default:
+                throw new \Exception('Operator ' . $operator . ' not found.');
+        }
+
+    }
+
+    public function update($registry, $operator, $value)
+    {
+        if($operator === 'inc') {
+            if(!array_key_exists($registry,  $this->registry)) {
+                $this->registry[$registry] = $value;
+            } else {
+                $this->registry[$registry] = $this->registry[$registry] + $value;
+            }
+
+        } else {
+            if(!array_key_exists($registry,  $this->registry)) {
+                $this->registry[$registry] = $value;
+            } else {
+                $this->registry[$registry] = $this->registry[$registry] - $value;
+            }
+        }
+
+        if($this->registry[$registry] >= $this->allTimeHigh) {
+            $this->allTimeHigh = $this->registry[$registry];
+        }
+    }
+
+    public function getHighest()
+    {
+        return max($this->registry);
+    }
+
+    /**
+     * @param int $part
+     * @return mixed
+     * @throws \Exception
+     */
     public function run($part = 1)
     {
-        return "day8";
+        $input = $this->readInputFile(dirname(__FILE__));
+
+        if($part === 1) {
+            $this->setRegistry([]);
+            $this->setInstruction($input);
+            return $this->getHighest();
+        } else {
+            $this->setRegistry([]);
+            $this->setInstruction($input);
+            return $this->allTimeHigh;
+        }
+
     }
 }
