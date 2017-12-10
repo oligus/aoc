b1d819b5f13d62857a10bc68296fe6e4ce0c25d0
oligus <oli.gustafsson@gmail.com>
2017-12-10 21:27:21 +0100

    Aoc 2017 10 1-2

--- spec/2017/10/Aoc.spec.php
+++ spec/2017/10/Aoc.spec.php
@@ -7,8 +7,39 @@ use AOC\Aoc2017\Day10\Aoc;
 describe('Advent of code', function() {
     $aoc = new Aoc();
 
-    it('Test run part 1', function() use ($aoc) {
-        expect($aoc->run(1))->toEql('day10');
+    xit('Test run part 1', function() use ($aoc) {
+        expect($aoc->run(1))->toEql(11375);
     });
 
+    it('Test run part 2', function() use ($aoc) {
+        expect($aoc->run(2))->toEql('e0387e2ad112b7c2ef344e44885fe4d8');
+    });
+
+    it('Test process', function() use ($aoc) {
+        expect($aoc->process([0, 1, 2, 3, 4], [3, 4, 1, 5]))->toEql([3, 4, 2, 1, 0]);
+    });
+
+    it('Test process ascii', function() use ($aoc) {
+        expect($aoc->processAscii([0, 1, 2, 3, 4], [3, 4, 1, 5, 17, 31, 73, 47, 23]))->toEql([0, 2, 4, 3, 1]);
+    });
+
+    xit('Test hash', function() use ($aoc) {
+        expect($aoc->hash([3, 4, 1, 5, 17, 31, 73, 47, 23]))->toEql('7c000000000000000000000000000000');
+    });
+
+    it('Test process', function() use ($aoc) {
+        expect($aoc->convert('1,2,3'))->toEql([49, 44, 50, 44, 51, 17, 31, 73, 47, 23]);
+    });
+
+    it('Test merge', function() use ($aoc) {
+        expect($aoc->merge([0, 1, 2, 3, 4], [2, 1, 0], 0))->toEql([2,1,0,3,4]);
+        expect($aoc->merge([0, 1, 2, 3, 4], [2, 1, 0], 4))->toEql([1, 0, 2, 3, 2]);
+    });
+
+    it('Test get list', function() use ($aoc) {
+        expect($aoc->getList([0, 1, 2, 3, 4], 3, 0))->toEql([0, 1, 2]);
+        expect($aoc->getList([0, 1, 2, 3, 4], 3, 2))->toEql([2, 3, 4]);
+        expect($aoc->getList([0, 1, 2, 3, 4], 7, 0))->toEql([0,1,2,3,4,0,1]);
+        expect($aoc->getList([0, 1, 2, 3, 4], 17, 0))->toEql([0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1]);
+    });
 });
--- src/2017/10/Aoc.php
+++ src/2017/10/Aoc.php
@@ -8,8 +8,123 @@ use AOC\Common\AbstractAoc;
 
 class Aoc extends AbstractAoc
 {
+    /**
+     * @param $list
+     * @param $lengths
+     * @return mixed
+     */
+    public function process($list, $lengths)
+    {
+        $skip = 0;
+        $start = 0;
+
+        foreach($lengths as $length) {
+            $sublist = array_reverse($this->getList($list, $length, $start % count($list)));
+            $list = $this->merge($list, $sublist, $start % count($list));
+            $start += ($length + $skip);
+            $skip++;
+        }
+
+        return $list;
+    }
+
+    public function processAscii($list, $lengths)
+    {
+        $skip = 0;
+        $start = 0;
+
+        for ($rounds = 0; $rounds < 64; $rounds++) {
+            foreach ($lengths as $length) {
+                $sublist = array_reverse($this->getList($list, $length, $start % count($list)));
+                $list = $this->merge($list, $sublist, $start % count($list));
+                $start += ($length + $skip);
+                $skip++;
+            }
+        }
+
+        return $list;
+    }
+
+    public function hash($list)
+    {
+        $hash = '';
+        $chunks = array_chunk($list, 16);
+
+        for ($i = 0; $i < 16; $i++) {
+            $xor = $chunks[$i][0];
+            for ($j = 1; $j < 16; $j++) {
+                $xor ^= $chunks[$i][$j];
+            }
+            $hash .= sprintf('%02s', dechex($xor));
+        }
+
+        return $hash;
+    }
+
+    /**
+     * @param string $string
+     * @return array
+     */
+    public function convert(string $string): array
+    {
+        $result = [];
+
+        foreach(str_split($string) as $char) {
+            $result[] = ord($char);
+        }
+
+        return array_merge($result, [17, 31, 73, 47, 23]);
+    }
+
+    /**
+     * @param $list
+     * @param $part
+     * @param $start
+     * @return mixed
+     */
+    public function merge($list, $part, $start)
+    {
+        for($i = 0; $i < count($part); $i++) {
+            $list[$start % count($list)] = $part[$i];
+            $start++;
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param $list
+     * @param $length
+     * @param $start
+     * @return array
+     */
+    public function getList($list, $length, $start)
+    {
+        $result = [];
+
+        for($i = 0; $i < $length; $i++) {
+            $result[] = $list[$start % count($list)];
+            $start++;
+        }
+
+        return $result;
+    }
+
     public function run($part = 1)
     {
-        return "day10";
+        $list = range(0, 255);
+
+        if($part === 1) {
+            $input = $this->readInput(dirname(__FILE__));
+            $values = explode(',', $input);
+            $result = $this->process($list, $values);
+            return $result[0] * $result[1];
+        } else {
+            $input = $this->readInput(dirname(__FILE__));
+            $convert = $this->convert($input);
+            $result = $this->processAscii($list, $convert);
+            return $this->hash($result);
+        }
+
     }
 }
