e175fdba730c1d1ef88137eca0021e9ce03bf35a
oligus <oli.gustafsson@gmail.com>
2017-12-11 18:36:39 +0100

    Aoc 2017 10 1-2

--- spec/2017/11/Aoc.spec.php
+++ spec/2017/11/Aoc.spec.php
@@ -8,7 +8,22 @@ describe('Advent of code', function() {
     $aoc = new Aoc();
 
     it('Test run part 1', function() use ($aoc) {
-        expect($aoc->run(1))->toEql('day11');
+        expect($aoc->run(1))->toEql(812);
     });
 
+    it('Test run part 2', function() use ($aoc) {
+        expect($aoc->run(2))->toEql(1603);
+    });
+
+
+    it('Test step', function() use ($aoc) {
+        expect($aoc->step('ne'))->toEql([1, -1, 0]);
+    });
+
+    it('Test process', function() use ($aoc) {
+        expect($aoc->process(['ne', 'ne', 'ne']))->toEql(3);
+        expect($aoc->process(['ne','ne','sw','sw']))->toEql(0);
+        expect($aoc->process(['ne','ne','s','s']))->toEql(2);
+        expect($aoc->process(['se','sw','se','sw','sw']))->toEql(3);
+    });
 });
--- src/2017/11/Aoc.php
+++ src/2017/11/Aoc.php
@@ -8,8 +8,64 @@ use AOC\Common\AbstractAoc;
 
 class Aoc extends AbstractAoc
 {
+    /**
+     * @param $step
+     * @return array
+     */
+    public function step($step)
+    {
+        switch($step) {
+            case 'n' : return [1,  0, -1]; break;
+            case 'ne': return [1, -1,  0]; break;
+            case 'nw': return [0,  1, -1]; break;
+            case 's' : return [-1, 0,  1]; break;
+            case 'se': return [0, -1,  1]; break;
+            case 'sw': return [-1, 1,  0]; break;
+            default: return []; break;
+        }
+    }
+
+    public function process($steps)
+    {
+        $x = $y = $z = 0;
+
+        foreach($steps as $step) {
+            $result = $this->step($step);
+            $x = $x + $result[0];
+            $y = $y + $result[1];
+            $z = $z + $result[2];
+        }
+
+        return  (abs($x) + abs($y) + abs($z)) / 2;
+    }
+
+    public function furthest($steps)
+    {
+        $x = $y = $z = $furthest = 0;
+
+        foreach($steps as $step) {
+            $result = $this->step($step);
+            $x = $x + $result[0];
+            $y = $y + $result[1];
+            $z = $z + $result[2];
+
+            $distance =  (abs($x) + abs($y) + abs($z)) / 2;
+            $furthest = $distance > $furthest ? $distance : $furthest;
+        }
+
+        return $furthest;
+
+    }
+
     public function run($part = 1)
     {
-        return "day11";
+        $input = $this->readInput(dirname(__FILE__));
+        $values = explode(',', $input);
+
+        if($part === 1) {
+            return $this->process($values);
+        } else {
+            return $this->furthest($values);
+        }
     }
 }
